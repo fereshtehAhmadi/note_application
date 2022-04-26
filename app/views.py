0a306891dc3f6ea42b40e61e2c3af7896df67a63
freshtehAhmadi <freshtehahmadi79@gmail.com>
2022-04-26 19:19:13 +0430

    make home page

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(Request):
+    context = {
+        'notes': notes.objects.all(),
+    }
+    return render(Request, 'app/index.html', context)
+
