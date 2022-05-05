2b1fd4739bc542cabd209e2078c03925e28ec7f5
freshtehAhmadi <freshtehahmadi79@gmail.com>
2022-05-05 16:10:19 +0430

    edit urls

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from app import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home/<str:order>/', views.home, name= 'home_with_order'),
+    path('<str:order>/', views.home, name= 'home_with_order'),
     path('add/', views.add_note, name= 'add'),
     path('detail/<int:pk>/', views.detail_nots, name= 'detail'),
     path("edit/<int:pk>/", views.edit_note, name="edit"),
